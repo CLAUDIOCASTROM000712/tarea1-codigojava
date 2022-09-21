44f0fc2fea5fed436ed7020116377c501219cd3d
CLAUDIOCASTROM000712 <112443996+CLAUDIOCASTROM000712@users.noreply.github.com>
2022-09-21 18:43:01 -0500

    Add files via upload

---
+++ ClaudioPrograma.java
@@ -0,0 +1,114 @@
+class Claudioprograma{
+    public static void main(String args[]){
+        cliente.persona= (persona1) Cliente.getNombre("felix castro gonzales");
+        cliente.persona= (persona1) Cliente.getEdad(52);
+        cliente.persona= (persona1) Cliente.getDinero(900.50);
+        cliente.persona= (persona1) Cliente.getRfc("cagf700712387");
+       // System.out.println();
+       // System.out.println();
+       // System.out.println();
+       // System.out.println();
+
+    }
+
+}
+
+class Persona{
+    private String nombre;
+    private int edad;
+    private String rfc;
+    private float dinero;
+
+    private int noTrabajador;
+    private float sueldo;
+    private String area;
+    private String sucursal;
+    private int noSubalternos;
+
+    private Cliente cliente;
+    private Asesor asesor;
+    private Gerente gerente;
+
+    public Persona(){
+        nombre="felix castro gonzales";
+        edad=52;
+        rfc="cagf700712387";
+        dinero=900.50;
+        noTrabajador=16;
+        sueldo=300.50;
+        area="electronica";
+        sucursal="elcentro";
+        noSubalternos=2;
+
+        cliente=new Cliente();
+        asesor=new Asesor();
+        gerente=new Gerente();
+    }
+
+}
+
+class Cliente extends Persona{
+    super.Persona();
+    private String nombre;
+    private int edad;
+    private float dinero;
+    private String rfc;
+
+    public Cliente(String nombre,int edad,float dinero,String rfc){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.dinero=dinero;
+        this.rfc=rfc;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public float getDinero(){
+        return dinero;
+    }
+    public String getRfc(){
+        return rfc;
+    }
+
+}
+
+class Asesor extends Persona{
+    super.Persona();
+    private int noTrabajador;
+    private float sueldo;
+    private String area;
+
+    public Asesor(int noTrabajador,float sueldo,String area){
+        this.noTrabajador=noTrabajador;
+        this.sueldo=sueldo;
+        this.area=area;
+    }
+    public float getSueldo(){
+        return sueldo;
+    }
+    public String getArea(){
+        return area;
+    }
+
+}
+
+class Gerente extends Persona{
+    super.Persona();
+    private int noSubalternos;
+    private String sucursal;
+    
+    public Gerente(int noSubalternos,String sucursal){
+        this.noSubalternos=noSubalternos;
+        this.sucursal=sucursal;
+    }
+    public int getNoSubalternos(){
+        return noSubalternos;
+    }
+    public String getSurcusal(){
+        return sucursal;
+    }
+
+}
